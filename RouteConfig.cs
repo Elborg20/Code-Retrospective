7b668630e627ea9aefaf982d13b333fe3908a79a
Elborg20 <43588406+Elborg20@users.noreply.github.com>
2018-12-21 15:30:25 -0700

    Story 2933
    Create route map that maps default view of JPHires controller to Index

---
+++ RouteConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace JobPlacementDashboard
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "JPHire",
+                url: "jphires",
+                defaults: new { controller = "JPHires", action = "Index" }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Analytics", action = "Analytics", id = UrlParameter.Optional }
+            );  
+
+        }
+    }
+}
